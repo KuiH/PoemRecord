055ac2083ad64086b8245b40b3ec4f639eb836d6
KuiH <1392147039@qq.com>
2024-12-27 23:23:36 +0800

    add: 诗人和诗集界面

---
+++ components/poem-box/poem-box.js
@@ -0,0 +1,24 @@
+// components/poem-box/poem-box.js
+Component({
+
+  properties: {
+    title: {
+      type: String,
+      value: '默认标题' // 默认值
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- global.js
+++ global.js
@@ -8,13 +8,27 @@ const loadData = () => {
   globalData.poems = {
     唐: {
       李白: [
-        { title: "TITLE1", content: "wwww,wwww,wwwww,wwwww" },
-        { title: "TITLE2", content: "wwww,wwww,wwwww,wwwww" },
-        { title: "TITLE3", content: "wwww,wwww,wwwww,wwwww" },
-        { title: "TITL4E", content: "wwww,wwww,wwwww,wwwww" }
+        { title: "TITLE1TITLE1TITLE1", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "假装这是一首诗的名字", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "TITLE3·TITL4E啊啊啊啊", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "假装这是一首诗的名字而且不是一般长", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "假装这是一首诗的名字而且不是一般长", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "假装这是一首诗的名字而且不是一般长", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "假装这是一首诗的名字而且不是一般长", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "假装这是一首诗的名字而且不是一般长", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "假装这是一首诗的名字而且不是一般长", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "假装这是一首诗的名字而且不是一般长", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "假装这是一首诗的名字而且不是一般长", content: "wwww,wwww,wwwww,wwwww" },
+        { title: "假装这是一首诗的名字而且不是一般长", content: "wwww,wwww,wwwww,wwwww" }
       ],
       杜甫: [{ title: "TITLE杜甫", content: "wwww,wwww,wwwww,wwwww" }],
       白居易: [{ title: "TITLE白居易", content: "wwww,wwww,wwwww,wwwww" }],
+      白1: [{ title: "TITLE白居易", content: "wwww,wwww,wwwww,wwwww" }],
+      白2: [{ title: "TITLE白居易", content: "wwww,wwww,wwwww,wwwww" }],
+      白3: [{ title: "TITLE白居易", content: "wwww,wwww,wwwww,wwwww" }],
+      白4: [{ title: "TITLE白居易", content: "wwww,wwww,wwwww,wwwww" }],
+      白5: [{ title: "TITLE白居易", content: "wwww,wwww,wwwww,wwwww" }],
+      白6: [{ title: "TITLE白居易", content: "wwww,wwww,wwwww,wwwww" }],
     },
     宋: {
       苏轼: [{ title: "TITLES", content: "wwww,wwww,wwwww,wwwww" }]
